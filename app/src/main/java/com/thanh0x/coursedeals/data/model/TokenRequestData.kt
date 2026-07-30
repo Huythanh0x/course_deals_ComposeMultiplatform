f6781d74c882dfc70debc1a306cb6d54fc9f4462
huythanh0x <huythanh0x@gmail.com>
2026-07-30 14:01:45 +0700

    add SerializedName annotations to authentication and token data models

--- app/src/main/java/com/thanh0x/coursedeals/data/model/TokenRequestData.kt
+++ app/src/main/java/com/thanh0x/coursedeals/data/model/TokenRequestData.kt
@@ -1,6 +1,10 @@
 package com.thanh0x.coursedeals.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class TokenRequestData(
+    @SerializedName("accessToken")
     val accessToken: String,
+    @SerializedName("refreshToken")
     val refreshToken: String,
 )
