9248c9b712636c8aa35fff75a0d33a9e468ab752
huythanh0x <huythanh0x@gmail.com>
2026-08-01 00:40:42 +0700

    implement adaptive landscape layouts with NavigationRailView and update MainActivity for NavigationBarView compatibility

--- app/src/main/java/com/thanh0x/coursedeals/MainActivity.kt
+++ app/src/main/java/com/thanh0x/coursedeals/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationBarView
 import com.thanh0x.coursedeals.databinding.ActivityMainBinding
 import com.thanh0x.coursedeals.ui.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,7 +18,9 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         navController = Navigation.findNavController(this, R.id.fragmentMainNavigationHost)
-        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
+        (binding.bottomNavigationView as? NavigationBarView)?.let {
+            NavigationUI.setupWithNavController(it, navController)
+        }
     }
 
     override fun onNavigateUp(): Boolean {
